4b4a2317d15fe094c7e05869f2266e95043f214c
alumen2101 <alireza.keshavarz.d@gmail.com>
2025-03-10 15:58:38 +0330

    Add project files.

---
+++ RushHour.cpp
@@ -0,0 +1,12 @@
+﻿// RushHour.cpp : Defines the entry point for the application.
+//
+
+#include "RushHour.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
